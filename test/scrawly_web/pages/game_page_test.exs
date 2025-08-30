55fa11dc0296fca0cadf4cff4ee9537046c550c4
ken-kost <kenneth.kostresevic@gmail.com>
2025-08-30 13:29:59 +0200

    test: migrate from controller tests to LiveView page tests
    - Remove obsolete page_controller_test.exs
    - Add new test suites for HomePage and GamePage LiveViews
    - Update test structure to align with LiveView architecture

---
+++ test/scrawly_web/pages/game_page_test.exs
@@ -0,0 +1,44 @@
+defmodule ScrawlyWeb.Pages.GamePageTest do
+  use ExUnit.Case, async: true
+
+  alias ScrawlyWeb.Pages.GamePage
+
+  describe "init/3" do
+    test "initializes with room data" do
+      params = %{room_id: "123"}
+      component = GamePage.init(params, %Hologram.Component{}, nil)
+
+      assert component.state.room_id == "123"
+      assert component.state.room_name == "Room 123"
+      assert is_list(component.state.players)
+      assert component.state.new_message == ""
+      assert is_list(component.state.chat_messages)
+    end
+  end
+
+  describe "action/3" do
+    test "update_message updates the message" do
+      component = %Hologram.Component{state: %{new_message: ""}}
+      result = GamePage.action("update_message", %{"value" => "Hello"}, component)
+
+      assert result.state.new_message == "Hello"
+    end
+
+    test "send_message adds message and clears input" do
+      component = %Hologram.Component{state: %{new_message: "Test message", chat_messages: []}}
+      result = GamePage.action("send_message", %{}, component)
+
+      assert result.state.new_message == ""
+      assert length(result.state.chat_messages) == 1
+      assert hd(result.state.chat_messages).message == "Test message"
+    end
+
+    test "send_message ignores empty messages" do
+      component = %Hologram.Component{state: %{new_message: "   ", chat_messages: []}}
+      result = GamePage.action("send_message", %{}, component)
+
+      assert result.state.new_message == "   "
+      assert length(result.state.chat_messages) == 0
+    end
+  end
+end
---
+++ test/scrawly_web/pages/home_page_test.exs
@@ -0,0 +1,40 @@
+defmodule ScrawlyWeb.Pages.HomePageTest do
+  use ExUnit.Case, async: true
+
+  alias ScrawlyWeb.Pages.HomePage
+
+  describe "init/3" do
+    test "initializes with default state" do
+      component = HomePage.init(%{}, %Hologram.Component{}, nil)
+
+      assert component.state.show_create_room == false
+      assert component.state.new_room_name == ""
+      assert is_list(component.state.rooms)
+      assert length(component.state.rooms) > 0
+    end
+  end
+
+  describe "action/3" do
+    test "show_create_room sets show_create_room to true" do
+      component = %Hologram.Component{state: %{show_create_room: false}}
+      result = HomePage.action("show_create_room", %{}, component)
+
+      assert result.state.show_create_room == true
+    end
+
+    test "hide_create_room resets modal state" do
+      component = %Hologram.Component{state: %{show_create_room: true, new_room_name: "test"}}
+      result = HomePage.action("hide_create_room", %{}, component)
+
+      assert result.state.show_create_room == false
+      assert result.state.new_room_name == ""
+    end
+
+    test "update_room_name updates the room name" do
+      component = %Hologram.Component{state: %{new_room_name: ""}}
+      result = HomePage.action("update_room_name", %{"value" => "New Room"}, component)
+
+      assert result.state.new_room_name == "New Room"
+    end
+  end
+end
