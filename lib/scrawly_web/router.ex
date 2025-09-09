87316fb28545ad02d517097cf7ce727f3fbf3884
ken-kost <kenneth.kostresevic@gmail.com>
2025-09-09 19:21:20 +0200

    feat(web): integrate authentication into routing and web core
    - Add AshAuthentication.Plug.Helpers to router imports
    - Update session retrieval from load_from_session to retrieve_from_session
    - Enhance PageController with authentication checks
      - Redirect unauthenticated users to register page
      - Store return path for post-auth redirects
      - Pass authenticated requests to Hologram
    - Reorganize router to separate auth routes from Hologram routes
    - Remove LiveView session setup in favor of Hologram integration

--- lib/scrawly_web/router.ex
+++ lib/scrawly_web/router.ex
@@ -12,7 +12,7 @@ defmodule ScrawlyWeb.Router do
     plug :put_root_layout, html: {ScrawlyWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug :load_from_session
+    plug :retrieve_from_session
   end
 
   pipeline :api do
@@ -24,27 +24,12 @@ defmodule ScrawlyWeb.Router do
   scope "/", ScrawlyWeb do
     pipe_through :browser
 
-    ash_authentication_live_session :authenticated_routes do
-      # in each liveview, add one of the following at the top of the module:
-      #
-      # If an authenticated user must be present:
-      # on_mount {ScrawlyWeb.LiveUserAuth, :live_user_required}
-      #
-      # If an authenticated user *may* be present:
-      # on_mount {ScrawlyWeb.LiveUserAuth, :live_user_optional}
-      #
-      # If an authenticated user must *not* be present:
-      # on_mount {ScrawlyWeb.LiveUserAuth, :live_no_user}
-    end
-  end
-
-  scope "/", ScrawlyWeb do
-    pipe_through :browser
+    # Hologram routes - authentication handled in PageController
+    # get "/", PageController, :show
+    # get "/game/*path", PageController, :show
 
     auth_routes AuthController, Scrawly.Accounts.User, path: "/auth"
     sign_out_route AuthController
-    # Home page route
-    get "/", PageController, :show
 
     # Remove these if you'd like to use your own authentication views
     sign_in_route register_path: "/register",
